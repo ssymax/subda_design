4bb0af1c3df42035952ec143d1c731efebb21b05
ssymax <sszymass@gmail.com>
2024-02-16 11:56:07 +0100

    lenis scroll top fix

--- src/app/_components/atoms/lenisScroller.tsx
+++ src/app/_components/atoms/lenisScroller.tsx
@@ -2,13 +2,16 @@
 
 import Lenis from '@studio-freight/lenis';
 import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 export default function LenisScroller() {
+  const pathname = usePathname();
+
   useEffect(() => {
     const lenis = new Lenis();
-
+    if (pathname) lenis.scrollTo(0, { immediate: true });
     lenis.on('scroll', ScrollTrigger.update);
 
     gsap.ticker.add((time) => {
@@ -20,7 +23,7 @@ export default function LenisScroller() {
     return () => {
       lenis.destroy();
     };
-  }, []);
+  }, [pathname]);
 
   return <></>;
 }
