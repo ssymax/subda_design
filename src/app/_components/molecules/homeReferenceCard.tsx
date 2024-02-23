bbc87bcaac01c1f6fee33510c430dccbeb755edf
ssymax <sszymass@gmail.com>
2024-02-23 11:16:25 +0100

    reference card fix

--- src/app/_components/molecules/homeReferenceCard.tsx
+++ src/app/_components/molecules/homeReferenceCard.tsx
@@ -1,71 +1,15 @@
-'use client';
-
-import { useRef } from 'react';
-import gsap from 'gsap';
-import SplitType from 'split-type';
 import { HomeReferenceCardProps } from '@/components/types';
-import { useGSAP } from '@gsap/react';
 import styles from '@/styles/molecules/homeReferenceCard.module.scss';
 
 export default function HomeReferenceCard({
   name,
   description,
   place,
-  id,
 }: HomeReferenceCardProps) {
-  const descriptionRef = useRef<HTMLSpanElement | null>(null);
-  const nameRef = useRef<HTMLSpanElement | null>(null);
-
-  useGSAP(
-    () => {
-      if (!descriptionRef.current) return;
-      if (!nameRef.current) return;
-      const text = SplitType.create(descriptionRef.current, { types: 'lines' });
-      const { lines } = text;
-      const tl = gsap.timeline();
-
-      if (tl.isActive()) return;
-      tl.clear();
-
-      tl.fromTo(
-        lines,
-        {
-          y: 20,
-          autoAlpha: 0,
-        },
-        {
-          y: 0,
-          autoAlpha: 1,
-          stagger: 0.1,
-          duration: 0.6,
-          ease: 'power4.out',
-        },
-      ).fromTo(
-        nameRef.current,
-        {
-          x: -20,
-          autoAlpha: 0,
-          duration: 0.2,
-        },
-        {
-          x: 0,
-          autoAlpha: 1,
-          onComplete: () => {
-            text.revert();
-            tl.kill();
-          },
-        },
-      );
-    },
-    { dependencies: [id] },
-  );
-
   return (
     <div className={styles.card}>
-      <span className={styles.description} ref={descriptionRef}>
-        {description}
-      </span>
-      <span className={styles.name} ref={nameRef}>{`${name}, ${place}`}</span>
+      <span className={styles.description}>{description}</span>
+      <span className={styles.name}>{`${name}, ${place}`}</span>
     </div>
   );
 }
--- src/app/_components/organisms/homeReferences.tsx
+++ src/app/_components/organisms/homeReferences.tsx
@@ -3,8 +3,11 @@
 'use client';
 
 import { useState } from 'react';
-import HomeHeader from '@/components/atoms/homeHeader';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import SplitType from 'split-type';
 import clsx from 'clsx';
+import HomeHeader from '@/components/atoms/homeHeader';
 import HomeReferenceCard from '@/components/molecules/homeReferenceCard';
 import Line from '@/components/atoms/line';
 import Parallax from '@/components/atoms/parallax';
@@ -16,15 +19,60 @@ import bathroom from '../../../../public/bathroom.jpg';
 export default function HomeReferences() {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const handleClickNext = () =>
-    activeIndex === opinions.length - 1
-      ? setActiveIndex(0)
-      : setActiveIndex(activeIndex + 1);
+  const tl = gsap.timeline();
+  useGSAP(
+    () => {
+      const text = SplitType.create('[class*="description"]', { types: 'lines' });
+      const { lines } = text;
 
-  const handleClickPrev = () =>
-    activeIndex === 0
-      ? setActiveIndex(opinions.length - 1)
-      : setActiveIndex(activeIndex - 1);
+      if (tl.isActive()) return;
+      tl.clear();
+
+      tl.fromTo(
+        lines,
+        {
+          y: 20,
+          autoAlpha: 0,
+        },
+        {
+          y: 0,
+          autoAlpha: 1,
+          stagger: 0.1,
+          duration: 0.6,
+          ease: 'power4.out',
+        },
+      ).fromTo(
+        '[class*="name"]',
+        {
+          x: -20,
+          autoAlpha: 0,
+          duration: 0.1,
+        },
+        {
+          x: 0,
+          autoAlpha: 1,
+          onComplete: () => {
+            text.revert();
+            tl.kill();
+          },
+        },
+      );
+    },
+    { dependencies: [activeIndex] },
+  );
+
+  const handleClickNext = () => {
+    if (tl.isActive()) return;
+
+    if (activeIndex === opinions.length - 1) setActiveIndex(0);
+    else setActiveIndex(activeIndex + 1);
+  };
+
+  const handleClickPrev = () => {
+    if (tl.isActive()) return;
+    if (activeIndex === 0) setActiveIndex(opinions.length - 1);
+    else setActiveIndex(activeIndex - 1);
+  };
 
   const arrowButtonProps = {
     role: 'button',
